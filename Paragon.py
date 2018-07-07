5e60ee33d24add65be755dd2615405fe4899d3ef
Jesse Blackwell <zellkoubarzhad@gmail.com>
2018-07-07 00:08:57 -0400

    why doesn't this work?

--- Paragon.py
+++ Paragon.py
@@ -1,10 +1,28 @@
 # Paragon Control Software
+
+#Imports
+
+#import serial
 import serial
+
+#import time&sleep
 from time import sleep
+
+#import OS
+import os
+
+#specify serial port
+print('Chose a serial port')
+
+#list serial ports
+os.system('ls /dev/tty*')
+
+#input saved here
+serVar = input()
 
 #Open Serial Port
 with serial.Serial() as ser:
-    ser.port = '/dev/ttyS0'
+   # ser.port = 'serVar'
     ser.baudrate = 1200
     ser.bytesize = EIGHTBITS
     ser.parity = PARITY_NONE
@@ -15,24 +33,37 @@ with serial.Serial() as ser:
     ser.write_timeout = 2
     ser.inter_byte_timeout = None
     ser.exclusive = True
+    ser.port = 'serVar'
     open()
     
 while True:
     data = ser.read(9999)
     if len(data) > 0:
-        print 'Paragon active', data
+        print('Paragon active', data)
 
     sleep(1)
-    print 'Ready'
+    print ('Ready')
+
+#clear screen before logo
+os.system('clear')
 
 # logo
 
-print "TEN-TEC Paragon Serial Interface Control Program"
-print "Copyright 1988"
-print "By"
-print "TEN-TEC, Inc."
-print "Ported to Python"
-print "By"
-print "The Hillbilly Hacker"
-print "Python 3 Version" #add date when finished
+print("TEN-TEC Paragon Serial Interface Control Program")
+print("Copyright 1988")
+print("By")
+print("TEN-TEC, Inc.")
+print("Ported to Python")
+print("By")
+print("KK4VFU")
+print("Python 3 Version") #add date when finished
+sleep(10)
+
+#clear sceen before main menu
+
+os.system('clear')
+
+#Main Menu
+
+
 
